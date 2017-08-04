3de0f379e6bd634aaef553542884836c59a48bd9
jl777 <jameslee777@yahoo.com>
2017-08-04 16:47:05 +0200

    Test

--- BreadWallet/BRMerkleBlock.m
+++ BreadWallet/BRMerkleBlock.m
@@ -30,6 +30,7 @@
 #define MAX_TIME_DRIFT    (2*60*60)     // the furthest in the future a block is allowed to be timestamped
 //#define MAX_PROOF_OF_WORK 0x1d00ffffu   // highest value for difficulty target (higher values are less difficult)
 extern uint32_t MAX_PROOF_OF_WORK;
+extern int32_t COIN_IS_KMD;
 #define TARGET_TIMESPAN   (14*24*60*60) // the targeted timespan between difficulty target adjustments
 
 // from https://en.bitcoin.it/wiki/Protocol_specification#Merkle_Trees
